1ccaaff45fb24d60057a32f20d274501791dfe16
Aditya <s.adityaraj2204@gmail.com>
2025-01-30 23:33:59 +0530

    Make the search bar interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -13,7 +13,25 @@ export function changeCartHTML() {
 function renderProductsGrid() {
   let productsHTML = '';
 
-  products.forEach((product) => {
+  const url = new URL(window.location.href);
+  const search = url.searchParams.get('search');
+
+  let filteredProducts = products;
+
+  if (search) {
+    filteredProducts = products.filter((product) => {
+      let matchingKeyword = false;
+
+      product.keywords.forEach((keyword) => {
+        if(keyword.toLowerCase().includes(search.toLowerCase())) {
+          matchingKeyword = true;
+        }
+      });
+      return matchingKeyword || product.name.toLowerCase().includes(search.toLowerCase());
+    });
+  }
+
+  filteredProducts.forEach((product) => {
     productsHTML += `
         <div class="product-container">
           <div class="product-image-container">
@@ -92,4 +110,19 @@ function renderProductsGrid() {
     
     });
   });
+
+  document.querySelector('.js-search-bar').addEventListener('keydown', (event) => {
+    if(event.key == 'Enter') {
+      showSearch();
+    }
+  });
+
+  document.querySelector('.js-search-button').addEventListener('click', () => {
+    showSearch();
+  });
+
+  function showSearch() {
+    const search = document.querySelector('.js-search-bar').value;
+    window.location.href = `amazon.html?search=${search}`;
+  }
 }
