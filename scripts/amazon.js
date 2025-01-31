10446e370c3fdbda4e3d14fa485ab12dd000b977
Aditya <s.adityaraj2204@gmail.com>
2025-01-31 18:50:24 +0530

    Update the amazon.html name to index.html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -123,6 +123,6 @@ function renderProductsGrid() {
 
   function showSearch() {
     const search = document.querySelector('.js-search-bar').value;
-    window.location.href = `amazon.html?search=${search}`;
+    window.location.href = `index.html?search=${search}`;
   }
 }
